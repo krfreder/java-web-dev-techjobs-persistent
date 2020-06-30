300106872cf300fe0d02b33e668526d27f1aa2bb
Kyleah <krfrederic@gmail.com>
2020-06-29 19:53:27 -0500

    connected to SQL database

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
-
+SQL table job: column name, data type:
+- id, null
+- employer, null
+- name, null
+- skills, null
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
